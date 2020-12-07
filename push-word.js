b124d391eeaf5f3858741522dd714b2fffd6f2a6
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-12-07 14:37:14 +0900

    Change input way
    html form으로 입력 받는 방식에서 node JS의 readline으로 활용

--- push-word.js
+++ push-word.js
@@ -1,14 +1,17 @@
-const form = document.querySelector("#js-form");
-const input = document.querySelector("#js-input");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-form.addEventListener("submit", handleSubmit);
-
-function handleSubmit(event) {
-  event.preventDefault();
-  const [str, cnt, dir] = input.value.split(" ");
+rl.on("line", function (line) {
+  const [str, cnt, dir] = line.split(" ");
   moveChar(str, cnt, dir.toUpperCase());
-  input.value = "";
-}
+  rl.close();
+}).on("close", function () {
+  process.exit();
+});
+
 function moveChar(str, count, dir) {
   const strLen = str.length;
   if (count < 0) {
