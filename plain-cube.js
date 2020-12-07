0f820ef75e180e90e65bd3905bced958401015dc
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-12-07 15:14:37 +0900

    Feature. split string with '
    입력받은 문자열을 ' 까지 포함해서 배열로 split하기

--- plain-cube.js
+++ plain-cube.js
@@ -9,7 +9,30 @@
 > B' 가장 아랫줄을 왼쪽으로 한 칸 밀기 GBB -> BBG
 > Q  Bye~를 출력하고 프로그램을 종료한다.
 */
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
+rl.on("line", function (line) {
+  const charList = splitString(line);
+  //위에 배열 forEach로 아니면 for문으로 array 변경해 출력하는 함수
+  // 위 반복문 안에서 Q 나오면 rl.close();
+}).on("close", function () {
+  process.exit();
+});
+
+function splitString(str) {
+  let strList = str.split("");
+  for (let i = 0; i < strList.length; i++) {
+    if (strList[i] === "'") strList[i - 1] += "'";
+  }
+  strList = strList.filter((char) => char !== "'");
+  return strList;
+}
+
+splitString("UU'RR'");
 let testArr = [
   ["R", "R", "W"],
   ["G", "C", "W"],
