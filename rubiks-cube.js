b0a309b5c68aba04970c9157c1d5446fdf62e0da
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-12-08 12:18:40 +0900

    Create. movetype data

--- rubiks-cube.js
+++ rubiks-cube.js
@@ -1,17 +1,54 @@
-const cube = {
-  up: {
-    data: Array.from(Array(3), () => new Array(3).fill('B')),
-    linked: [this.front, cube.right, cube.back, cube.left]
+let cube = {
+  up: Array.from(Array(3), () => new Array(3).fill("B")),
+  down: Array.from(Array(3), () => new Array(3).fill("R")),
+  front: Array.from(Array(3), () => new Array(3).fill("O")),
+  right: Array.from(Array(3), () => new Array(3).fill("G")),
+  left: Array.from(Array(3), () => new Array(3).fill("W")),
+  back: Array.from(Array(3), () => new Array(3).fill("Y")),
+};
+
+const DIR_TYPE = {
+  UP: { fixer: "row", fixIndex: 0 },
+  DOWN: { fixer: "row", fixIndex: 2 },
+  LEFT: { fixer: "column", fixIndex: 0 },
+  RIGHT: { fixer: "column", fixIndex: 2 },
+};
+
+let MOVE_TYPE = {
+  F: {
+    data: cube.front,
+    linked: [cube.up, cube.right, cube.down, cube.left],
+    linkedDir: [DIR_TYPE.DOWN, DIR_TYPE.LEFT, DIR_TYPE.UP, DIR_TYPE.RIGHT],
   },
-  down: { data: Array.from(Array(3), () => new Array(3).fill('R')) },
-  front: { data: Array.from(Array(3), () => new Array(3).fill('O')) },
-  right: { data: Array.from(Array(3), () => new Array(3).fill('G')) },
-  left: { data: Array.from(Array(3), () => new Array(3).fill('W')) },
-  back: { data: Array.from(Array(3), () => new Array(3).fill('Y')) }
+  R: {
+    data: cube.right,
+    linked: [cube.up, cube.back, cube.down, cube.front],
+    linkedDir: [DIR_TYPE.RIGHT, DIR_TYPE.LEFT, DIR_TYPE.DOWN, DIR_TYPE.DOWN],
+  },
+  L: {
+    data: cube.left,
+    linked: [cube.up, cube.front, cube.down, cube.back],
+    linkedDir: [DIR_TYPE.LEFT, DIR_TYPE.LEFT, DIR_TYPE.LEFT, DIR_TYPE.RIGHT],
+  },
+  B: {
+    data: cube.back,
+    linked: [cube.up, cube.left, cube.down, cube.right],
+    linkedDir: [DIR_TYPE.UP, DIR_TYPE.LEFT, DIR_TYPE.DOWN, DIR_TYPE.RIGHT],
+  },
+  U: {
+    data: cube.up,
+    linked: [cube.front, cube.left, cube.back, cube.right],
+    linkedDir: [DIR_TYPE.UP, DIR_TYPE.UP, DIR_TYPE.UP, DIR_TYPE.UP],
+  },
+  D: {
+    data: cube.down,
+    linked: [cube.front, cube.right, cube.back, cube.left],
+    linkedDir: [DIR_TYPE.DOWN, DIR_TYPE.DOWN, DIR_TYPE.DOWN, DIR_TYPE.DOWN],
+  },
 };
 
 function splitString(str) {
-  let strList = str.split('').map((v) => v.toUpperCase());
+  let strList = str.split("").map((v) => v.toUpperCase());
   for (let i = 0; i < strList.length; i++) {
     if (strList[i] === "'") strList[i - 1] += "'";
   }
@@ -28,4 +65,25 @@ function splitString(str) {
   return newStrList;
 }
 
-console.dir(cube, { depth: null });
+function moveCube(cube, type) {
+  type = MOVE_TYPE[type];
+  let newArr = [];
+  for (let i = 0; i < type.linked; i++) {
+    const linkedArr = makeNewArr(type.linked[i]);
+    newArr.push(linkedArr);
+  }
+}
+
+function makeNewArr(cube, dir) {
+  let newArr = [];
+  if (dir.fixer === "column") {
+    for (let i = 0; i < cube.length; i++) {
+      newArr.push(cube[dir.fixIndex][i]);
+    }
+  } else {
+    for (let i = 0; i < cube.length; i++) {
+      newArr.push(cube[i][dir.fixIndex]);
+    }
+  }
+  return newArr;
+}
