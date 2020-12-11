f84dd9e598272e69c497b72cedbbf9a8675cbf14
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-12-11 12:06:51 +0900

    Fix. 예외구문 추가

--- push-word.js
+++ push-word.js
@@ -1,15 +1,15 @@
 function moveChar(str, count, dir) {
   const strLen = str.length;
   if (count < 0) {
-    dir = dir === "R" ? "L" : "R";
+    dir = dir === 'R' ? 'L' : 'R';
     count *= -1;
   }
   count = (count * 1) % strLen;
-  if (dir === "L") count = strLen - count;
+  if (dir === 'L') count = strLen - count;
 
-  let strList = str.split("");
+  let strList = str.split('');
   strList = moveList(strList, count);
-  console.log(strList.join(""));
+  console.log(strList.join(''));
 }
 function moveOneIndex(arr) {
   const last = arr.pop();
@@ -24,16 +24,21 @@ function moveList(arr, count) {
   return list;
 }
 
-const readline = require("readline");
+const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
-
-rl.on("line", function (line) {
-  const [str, cnt, dir] = line.split(" ");
-  moveChar(str, cnt, dir.toUpperCase());
+console.log('문자열, 숫자, 방향을 공백으로 구분해 입력해주세요.');
+console.log('ex) apple 3 R ');
+rl.on('line', function (line) {
+  try {
+    const [str, cnt, dir] = line.split(' ');
+    moveChar(str, cnt, dir.toUpperCase());
+  } catch (error) {
+    console.log('잘못된 형식으로 입력했습니다.');
+  }
   rl.close();
-}).on("close", function () {
+}).on('close', function () {
   process.exit();
 });
